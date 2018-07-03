37227ece0d202db514694a26f2588233cf6e18d4
Alejandro Rodriguez <alex.rodriguez@conwet.com>
2018-07-03 13:03:16 +0200

    Create repository

---
+++ src/doc/userguide.md
@@ -0,0 +1,17 @@
+## Introduction
+
+Harvest data from a CrateDB instance
+
+## Settings
+
+## Wiring
+
+### Input Endpoints
+
+### Output Endpoints
+
+## Usage
+
+## Reference
+
+- [FIWARE Mashup](https://mashup.lab.fiware.org/)
---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+/*
+ * crateDB-harvester
+ * https://github.com/mognom/crateDB-harvester-operator
+ *
+ * Copyright (c) 2018 CoNWeT
+ * Licensed under the MIT license.
+ */
+
+(function () {
+
+    "use strict";
+
+    MashupPlatform.prefs.registerCallback(function (new_preferences) {
+
+    }.bind(this));
+
+    /* test-code */
+
+    /* end-test-code */
+
+})();
