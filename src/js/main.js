9f3167cbd8bd67b28987ab94d0065b0c8c0d21c1
Alejandro Rodriguez <alex.rodriguez@conwet.com>
2018-07-03 13:32:28 +0200

    Initial version

--- src/js/main.js
+++ src/js/main.js
@@ -10,12 +10,52 @@
 
     "use strict";
 
-    MashupPlatform.prefs.registerCallback(function (new_preferences) {
+    var init = function init() {
+        MashupPlatform.wiring.registerStatusCallback(pushInfo);
 
-    }.bind(this));
+        // On preferences update
+        MashupPlatform.prefs.registerCallback(updatePrefs);
+        updatePrefs();
+    };
+    var server, query, data;
+    var updatePrefs = function updatePrefs() {
+        server = MashupPlatform.prefs.get("server").trim();
+        query = MashupPlatform.prefs.get("query").trim();
 
-    /* test-code */
+        harvestNewData();
+    };
 
-    /* end-test-code */
+    var harvestNewData = function harvestNewData() {
+        var headers = {};
+        headers["Content-Type"] = "application/json; charset=UTF-8";
+        MashupPlatform.http.makeRequest(server + "/_sql", {
+            method: 'POST',
+            supportsAccessControl: false,
+            postBody: JSON.stringify({stmt: query}),
+            requestHeaders: headers,
+            onSuccess: function (response) {
+                // If there's no data, its probably the last page.
+                var res = JSON.parse(response.responseText);
 
+                // Build response format
+                var cols = res.cols;
+                data = [];
+                data = res.rows.map(function (e) {
+                    var result = {};
+                    cols.forEach(function (col, i) {
+                        result[col] = e[i];
+                    });
+                    return result;
+                });
+
+                pushInfo();
+            }
+        });
+    };
+
+    var pushInfo = function pushInfo() {
+        MashupPlatform.wiring.pushEvent("output", data);
+    };
+
+    init();
 })();
